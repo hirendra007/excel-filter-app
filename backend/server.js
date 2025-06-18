fc4eab1173876f302b9a4dd0827c077f6afe5a0e
Hirendra B <hirendrabalaji3@gmail.com>
2025-06-19 00:27:45 +0530

    adjsankjs

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,7 @@ if (!process.env.MONGO_URI) {
   process.exit(1); // stop execution
 }
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch((err) => {
   console.error('❌ MongoDB connection error:', err);
